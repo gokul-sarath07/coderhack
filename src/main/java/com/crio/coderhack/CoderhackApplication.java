148cbae7adc961b014ccec23db69f698240887a2
Gokul Sarath <gokulsarath05@gmail.com>
2024-10-02 00:32:28 +0530

    mongodb connection added.

--- src/main/java/com/crio/coderhack/CoderhackApplication.java
+++ src/main/java/com/crio/coderhack/CoderhackApplication.java
@@ -2,8 +2,10 @@ package com.crio.coderhack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class CoderhackApplication {
 
 	public static void main(String[] args) {
